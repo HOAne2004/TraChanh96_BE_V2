2c12ec9a442bc47383a12d05de5fe7490af2be3f
HOAn04 <lehuyhoan04@gmail.com>
2025-12-31 20:35:24 +0700

    Update 2.2. Add connect vercel in Program.cs

--- drinking-be-v2/Program.cs
+++ drinking-be-v2/Program.cs
@@ -216,7 +216,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("http://localhost:5173", "https://tra-chanh-96.vercel.app")
                   .AllowAnyMethod()
                   .AllowAnyHeader()
                   .AllowCredentials();
